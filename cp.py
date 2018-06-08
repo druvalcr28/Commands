e5c1356f6ac2b2eae661673769cab5445dfecd8b
druvalcr28 <bim2016501@iiita.ac.in>
2018-06-08 23:52:02 +0530

    add directory copy

--- cp.py
+++ cp.py
@@ -11,7 +11,7 @@ def main():
         print("Enter a destination file or directory")
         sys.exit()
 
-    elif S_ISDIR(mode):
+    elif S_ISDIR(mode) and (sys.argv[1] != "-R"):   #OPTIMIZE IT
         for f in sys.argv[1:len(sys.argv)-1]:
             src_file = open(f,"r")
             pathname = os.path.join(last_argument,f)
@@ -24,9 +24,30 @@ def main():
         for line in src_file:
             copyfile(src_file,dest_file)
 
+    elif (sys.argv[1] == "-R") and (S_ISDIR(os.stat(sys.argv[2])[ST_MODE])):    #ASSUMING THAT DEST IS ALREADY A DIR 
+        os.mkdir(os.path.join(sys.argv[3],sys.argv[2]))
+        copydir(sys.argv[2],os.path.join(sys.argv[3],sys.argv[2]))
+    
     else:
         print("cannot execute command")
-        sys.exit()        
+        sys.exit()  
+
+def copydir(src_dir,dest_dir):
+    for f in os.listdir(src_dir):
+        pathname = os.path.join(src_dir,f)
+        mode = os.stat(pathname)[ST_MODE]
+
+        if S_ISDIR(mode):
+            os.mkdir(os.path.join(dest_dir,f))
+            copydir(pathname,os.path.join(dest_dir,f))
+        elif S_ISREG(mode):
+            src_file = open(pathname,"r")
+            pn = os.path.join(dest_dir,f)
+            dest_file = open(pn,"w+")
+            copyfile(src_file,dest_file)
+        else:
+            print("cannot execute command")
+            sys.exit()
 
 def copyfile(src_file,dest_file):
     for line in src_file:
